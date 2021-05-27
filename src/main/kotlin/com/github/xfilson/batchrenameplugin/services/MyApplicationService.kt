7d09b302df112fa823fd44bbef232f0fce07110b
GitHub Action <action@github.com>
2021-05-27 10:06:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/xfilson/batchrenameplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xfilson/batchrenameplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xfilson.batchrenameplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xfilson.batchrenameplugin.MyBundle
 
 class MyApplicationService {
 
